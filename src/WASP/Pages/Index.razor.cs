455d8158901f84d22ad2690d283bcacb3781ef1a
Gina Therese Hvidsten <gthvidsten@whitestone.no>
2022-08-02 20:03:30 +0200

    Added manual firing of events to debug page

---
+++ src/WASP/Pages/Index.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using Whitestone.Cambion.Interfaces;
+using Whitestone.WASP.Common.Events;
+
+namespace Whitestone.WASP.Pages
+{
+    public partial class Index
+    {
+        [Inject]
+        private ICambion Cambion { get; set; } = null!;
+
+        private async void FireEvent_PlayNextTrack_Click()
+        {
+            await Cambion.PublishEventAsync(new PlayNextTrack());
+        }
+        private async void FireEvent_StartStreaming_Click()
+        {
+            await Cambion.PublishEventAsync(new StartStreaming());
+        }
+        private async void FireEvent_StopStreaming_Click()
+        {
+            await Cambion.PublishEventAsync(new StopStreaming());
+        }
+    }
+}
