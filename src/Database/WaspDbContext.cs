54614ec8efdf46cb54ffbd079c49b02f5e67afb3
Gina Therese Hvidsten <gthvidsten@whitestone.no>
2022-08-08 19:44:06 +0200

    Added skeleton DbContext

---
+++ src/Database/WaspDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Whitestone.WASP.Database
+{
+    public class WaspDbContext : DbContext
+    {
+        public WaspDbContext(DbContextOptions<WaspDbContext> options) : base(options)
+        {
+        }
+    }
+}
