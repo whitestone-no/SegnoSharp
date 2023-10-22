45f13cc75f66ac5a5a106f1ba4748711e9bf2e83
Gina Therese Hvidsten <gthvidsten@whitestone.no>
2023-10-22 20:27:24 +0200

    Added Components Razor Class Library with initial ProgressBar component

---
+++ src/Components/ProgressBar.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Whitestone.SegnoSharp.Components
+{
+    public partial class ProgressBar
+    {
+        [Parameter]
+        public int CurrentStep { get; set; }
+
+        [Parameter]
+        public EventCallback<int> CurrentStepChanged { get; set; }
+
+        [Parameter]
+        public int TotalSteps { get; set; }
+
+        [Parameter]
+        public EventCallback<int> TotalStepsChanged { get; set; }
+
+        private decimal Percentage => TotalSteps == 0 ? 0 : Math.Round((decimal)CurrentStep / TotalSteps * 100);
+    }
+}
