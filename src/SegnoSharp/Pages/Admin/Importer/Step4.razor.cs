94a82113edc956929187316e7b3c63299026ee83
Gina Therese Hvidsten <gthvidsten@whitestone.no>
2023-10-22 20:28:37 +0200

    Added usage of ProgressBar component with inital skeleton Importer Step 4 page

---
+++ src/SegnoSharp/Pages/Admin/Importer/Step4.razor.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+
+namespace Whitestone.SegnoSharp.Pages.Admin.Importer
+{
+    public partial class Step4
+    {
+        private int TotalNumberOfSteps { get; set; } = 10;
+        private int CurrentStep { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Task.Run(UpdatePercent);
+        }
+
+        private async Task UpdatePercent()
+        {
+            for (var i = 0; i <= 10; i++)
+            {
+                CurrentStep = i;
+                
+                await InvokeAsync(StateHasChanged);
+
+                await Task.Delay(500);
+            }
+        }
+    }
+}
