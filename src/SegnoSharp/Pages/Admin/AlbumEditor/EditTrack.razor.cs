fb205fef02f2cefdff296ec457c92ecb8af9e9a9
Gina Therese Hvidsten <gthvidsten@whitestone.no>
2023-11-07 22:14:36 +0100

    Added editing of track data

--- src/SegnoSharp/Pages/Admin/AlbumEditor/EditTrack.razor.cs
+++ src/SegnoSharp/Pages/Admin/AlbumEditor/EditTrack.razor.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.JSInterop;
 using Whitestone.SegnoSharp.Database;
 using Whitestone.SegnoSharp.Database.Models;
 
@@ -14,10 +16,15 @@ namespace Whitestone.SegnoSharp.Pages.Admin.AlbumEditor
         [Parameter] public int Id { get; set; }
         [Inject] private IDbContextFactory<SegnoSharpDbContext> DbFactory { get; set; }
         [Inject] private NavigationManager NavigationManager { get; set; }
+        [Inject] private IJSRuntime JsRuntime { get; set; }
 
         private SegnoSharpDbContext DbContext { get; set; }
         private Track Track { get; set; }
         private List<PersonGroup> PersonGroups { get; set; }
+        private int SelectedPersonGroupId { get; set; }
+        private bool Invalid => string.IsNullOrEmpty(Track.Title) || (Track.TrackStreamInfo != null && string.IsNullOrEmpty(Track.TrackStreamInfo.FilePath));
+
+        private int _originalTrackNumber = 0;
 
         protected override async Task OnInitializedAsync()
         {
@@ -25,11 +32,16 @@ namespace Whitestone.SegnoSharp.Pages.Admin.AlbumEditor
 
             Track = await DbContext.Tracks
                 .Include(t => t.Disc).ThenInclude(d => d.Album)
+                .Include(t => t.TrackStreamInfo)
+                .Include(t => t.TrackPersonGroupPersonRelations).ThenInclude(r => r.Persons)
+                .Include(t => t.TrackPersonGroupPersonRelations).ThenInclude(r => r.PersonGroup)
                 .FirstOrDefaultAsync(t => t.Id == Id);
 
             PersonGroups = await DbContext.PersonGroups
-                .Where(g => g.Type == PersonGroupType.Album)
+                .Where(g => g.Type == PersonGroupType.Track)
                 .ToListAsync();
+
+            _originalTrackNumber = Track.TrackNumber;
         }
 
         private async Task Close(bool save = false)
@@ -37,6 +49,43 @@ namespace Whitestone.SegnoSharp.Pages.Admin.AlbumEditor
             if (save)
             {
                 await DbContext.SaveChangesAsync();
+
+                if (Track.TrackNumber != _originalTrackNumber)
+                {
+                    Disc disc = await DbContext.Discs
+                        .Include(d => d.Tracks)
+                        .FirstOrDefaultAsync(d => d.Id == Track.Disc.Id);
+
+                    if (Track.TrackNumber <= 0)
+                    {
+                        Track.TrackNumber = 1;
+                    }
+                    else if (Track.TrackNumber > disc.Tracks.Count)
+                    {
+                        Track.TrackNumber = (ushort)disc.Tracks.Count;
+                    }
+
+                    if (Track.TrackNumber != _originalTrackNumber)
+                    {
+                        if (Track.TrackNumber > _originalTrackNumber)
+                        {
+                            foreach (Track moveTrack in disc.Tracks.Where(t => t.TrackNumber <= Track.TrackNumber && t.TrackNumber > _originalTrackNumber && t.Id != Track.Id))
+                            {
+                                moveTrack.TrackNumber = (ushort)(moveTrack.TrackNumber - 1);
+                            }
+
+                        }
+                        else if (Track.TrackNumber < _originalTrackNumber)
+                        {
+                            foreach (Track moveTrack in disc.Tracks.Where(t => t.TrackNumber < _originalTrackNumber && t.TrackNumber >= Track.TrackNumber && t.Id != Track.Id))
+                            {
+                                moveTrack.TrackNumber = (ushort)(moveTrack.TrackNumber + 1);
+                            }
+                        }
+
+                        await DbContext.SaveChangesAsync();
+                    }
+                }
             }
 
             NavigationManager.NavigateTo($"/admin/albums/{Track.Disc.Album.Id}");
@@ -54,6 +103,64 @@ namespace Whitestone.SegnoSharp.Pages.Admin.AlbumEditor
             {
                 DbContext?.Dispose();
             }
+        }
+
+        private async Task OnBeforeInternalNavigation(LocationChangingContext context)
+        {
+            if (DbContext.ChangeTracker.HasChanges())
+            {
+                var confirmed = await JsRuntime.InvokeAsync<bool>("confirm", "You have unsaved changes. Are you sure you want to close?");
+                if (!confirmed)
+                {
+                    context.PreventNavigation();
+                }
+            }
+        }
+
+        private async Task<IEnumerable<Person>> ExecutePersonSearch(string searchTerm)
+        {
+            var p = new Person { TagName = searchTerm };
+
+            return await DbContext.Persons
+                .Where(pp => EF.Functions.Like(pp.LastName, "%" + searchTerm + "%") || EF.Functions.Like(pp.FirstName, "%" + searchTerm + "%"))
+                .ToListAsync();
+        }
+
+        private void AddPersonGroup()
+        {
+            Track.TrackPersonGroupPersonRelations.Add(new TrackPersonGroupPersonRelation
+            {
+                PersonGroup = PersonGroups.First(pg => pg.Id == SelectedPersonGroupId)
+            });
+        }
+
+        private void RemovePersonGroup(TrackPersonGroupPersonRelation personGroupRelation)
+        {
+            Track.TrackPersonGroupPersonRelations.Remove(personGroupRelation);
+        }
+
+        private void AddStreamInfo()
+        {
+            Track.TrackStreamInfo = new TrackStreamInfo();
+        }
+
+        private void RemoveStreamInfo()
+        {
+            Track.TrackStreamInfo = null;
+        }
+
+        private async Task DeleteTrack()
+        {
+            var confirmed = await JsRuntime.InvokeAsync<bool>("confirm", "Are you sure you want to delete this track? This operation cannot be undone!");
+            if (!confirmed)
+            {
+                return;
+            }
+
+            DbContext.Tracks.Remove(Track);
+            await DbContext.SaveChangesAsync();
+
+            NavigationManager.NavigateTo($"/admin/albums/{Track.Disc.Album.Id}");
         }
     }
 }
