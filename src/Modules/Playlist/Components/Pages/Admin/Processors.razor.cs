fbf0d5e8b65861493ebb6de90b7ed4de5455bb3b
Gina Therese Hvidsten <gthvidsten@whitestone.no>
2025-01-26 22:20:56 +0100

    First draft  of a working Playlist Processors page

---
+++ src/Modules/Playlist/Components/Pages/Admin/Processors.razor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Whitestone.SegnoSharp.Common.Interfaces;
+
+namespace Whitestone.SegnoSharp.Modules.Playlist.Components.Pages.Admin
+{
+    public partial class Processors
+    {
+        [Inject] private IEnumerable<IPlaylistProcessor> PlaylistProcessors { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+        }
+
+        private void MoveProcessorUp(IPlaylistProcessor processor)
+        {
+            IPlaylistProcessor processorAbove = PlaylistProcessors.FirstOrDefault(p => p.Settings.SortOrder == processor.Settings.SortOrder + 1);
+            
+            if (processorAbove == null)
+            {
+                return;
+            }
+
+            processorAbove.Settings.SortOrder = (ushort)(processorAbove.Settings.SortOrder - 1);
+            processor.Settings.SortOrder = (ushort)(processor.Settings.SortOrder + 1);
+        }
+
+        private void MoveProcessorDown(IPlaylistProcessor processor)
+        {
+            IPlaylistProcessor processorBelow = PlaylistProcessors.FirstOrDefault(p => p.Settings.SortOrder == processor.Settings.SortOrder - 1);
+
+            if (processorBelow == null)
+            {
+                return;
+            }
+
+            processorBelow.Settings.SortOrder = (ushort)(processorBelow.Settings.SortOrder + 1);
+            processor.Settings.SortOrder = (ushort)(processor.Settings.SortOrder - 1);
+        }
+    }
+}
