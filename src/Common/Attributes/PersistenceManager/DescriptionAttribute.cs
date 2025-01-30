8f43023159b0a81ac0b901315030f2361f9c3528
Gina Therese Hvidsten <gthvidsten@whitestone.no>
2025-01-30 22:31:42 +0100

    Added Description attribute to enable custom texts to be displayed in playlist processor settings view

---
+++ src/Common/Attributes/PersistenceManager/DescriptionAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Whitestone.SegnoSharp.Common.Attributes.PersistenceManager
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class DescriptionAttribute(string description) : Attribute
+    {
+        public string Description { get; set; } = description;
+    }
+}
