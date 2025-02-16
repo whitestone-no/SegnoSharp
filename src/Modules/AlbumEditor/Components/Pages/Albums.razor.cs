455f1dd23ddea48a21c1b3c156ee23caf7e2857b
Gina Therese Hvidsten <8248605+gthvidsten@users.noreply.github.com>
2025-02-16 22:08:07 +0100

    Added generic pagination styling, and pagination to album page

--- src/Modules/AlbumEditor/Components/Pages/Albums.razor.cs
+++ src/Modules/AlbumEditor/Components/Pages/Albums.razor.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
@@ -12,33 +13,68 @@ namespace Whitestone.SegnoSharp.Modules.AlbumEditor.Components.Pages
     {
         [Inject] private IDbContextFactory<SegnoSharpDbContext> DbFactory { get; set; }
 
-        private List<Album> AlbumList { get; set; } = new();
-        private SearchModel SearchModel { get; set; } = new();
+        private List<Album> AlbumList { get; set; } = [];
+        private SearchModel SearchModel { get; } = new();
+
+        private const int PageSize = 10;
+        private int TotalPages { get; set; }
+        private int CurrentPage { get; set; } = 1;
 
         protected override async Task OnInitializedAsync()
         {
             await using SegnoSharpDbContext dbContext = await DbFactory.CreateDbContextAsync();
+
+            TotalPages = (int)Math.Ceiling(await dbContext.Albums
+                .AsNoTracking()
+                .CountAsync() / (double)PageSize);
 
             AlbumList = await dbContext.Albums
                 .AsNoTracking()
                 .Include(a => a.Discs).ThenInclude(d => d.Tracks)
                 .AsSplitQuery()
                 .OrderByDescending(a => a.Added)
-                .Take(10)
+                .Skip(PageSize * (CurrentPage - 1))
+                .Take(PageSize)
                 .ToListAsync();
         }
 
         private async Task DoSearch()
         {
+            CurrentPage = 1;
+
             await using SegnoSharpDbContext dbContext = await DbFactory.CreateDbContextAsync();
 
-            AlbumList = dbContext.Albums
+            TotalPages = (int)Math.Ceiling(await dbContext.Albums
+                .AsNoTracking()
+                .Where(a => EF.Functions.Like(a.Title, "%" + SearchModel.SearchString + "%"))
+                .CountAsync() / (double)PageSize);
+
+            AlbumList = await dbContext.Albums
                 .AsNoTracking()
                 .Include(a => a.Discs).ThenInclude(d => d.Tracks)
                 .AsSplitQuery()
                 .Where(a => EF.Functions.Like(a.Title, "%" + SearchModel.SearchString + "%"))
-                .OrderBy(a => a.Title)
-                .ToList();
+                .OrderByDescending(a => a.Added)
+                .Skip(PageSize * (CurrentPage - 1))
+                .Take(PageSize)
+                .ToListAsync();
+        }
+
+        private async Task OnPageChanged(int page)
+        {
+            CurrentPage = page;
+
+            await using SegnoSharpDbContext dbContext = await DbFactory.CreateDbContextAsync();
+
+            AlbumList = await dbContext.Albums
+                .AsNoTracking()
+                .Include(a => a.Discs).ThenInclude(d => d.Tracks)
+                .AsSplitQuery()
+                .Where(a => EF.Functions.Like(a.Title, "%" + SearchModel.SearchString + "%"))
+                .OrderByDescending(a => a.Added)
+                .Skip(PageSize * (CurrentPage - 1))
+                .Take(PageSize)
+                .ToListAsync();
         }
 
         private static string GetAlbumUrl(int id)
