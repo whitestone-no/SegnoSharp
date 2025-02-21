87a6152fe61f202f11e5032c6455bdccc84bccfa
Gina Therese Hvidsten <8248605+gthvidsten@users.noreply.github.com>
2025-02-21 16:52:22 +0100

    Add functionality, styling and pagination to history page. Tweak public playlist page to be more similar to history page.

--- src/Modules/Playlist/Components/Pages/History.razor.cs
+++ src/Modules/Playlist/Components/Pages/History.razor.cs
@@ -1,110 +1,125 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Whitestone.Cambion.Interfaces;
+using Whitestone.SegnoSharp.Common.Events;
+using Whitestone.SegnoSharp.Common.Interfaces;
 using Whitestone.SegnoSharp.Database;
 using Whitestone.SegnoSharp.Database.Models;
-using Whitestone.SegnoSharp.Modules.Playlist.Models;
+using Whitestone.SegnoSharp.Modules.Playlist.ViewModels;
 
 namespace Whitestone.SegnoSharp.Modules.Playlist.Components.Pages
 {
-    public partial class History : IDisposable
+    public partial class History : IEventHandler<PlaylistUpdated>
     {
         [Inject] private IDbContextFactory<SegnoSharpDbContext> DbFactory { get; set; }
+        [Inject] private ICambion Cambion { get; set; }
+        [Inject] private IHashingUtil HashingUtil { get; set; }
+        [Inject] private ISystemClock SystemClock { get; set; }
+        [Inject] private ILogger<Playlist> Logger { get; set; }
 
         private List<HistoryViewModel> HistoryItems { get; set; } = [];
+        public DateTime SelectedDate { get; set; }
+        public DateTime MinDate { get; set; }
 
-        private SegnoSharpDbContext _dbContext;
+        private const int HistoryPageSize = 10;
+        private int HistoryTotalPages { get; set; }
+        private int HistoryCurrentPage { get; set; } = 1;
 
-        protected override async Task OnInitializedAsync()
+
+        protected override void OnInitialized()
         {
-            _dbContext = await DbFactory.CreateDbContextAsync();
+            SelectedDate = SystemClock.Now;
 
-            List<StreamHistory> history = await _dbContext.StreamHistory
-                .Include(t => t.TrackStreamInfo)
-                .ThenInclude(t => t.Track)
-                .ThenInclude(t => t.TrackPersonGroupPersonRelations)
-                .ThenInclude(r => r.Persons)
-                .Include(t => t.TrackStreamInfo)
-                .ThenInclude(t => t.Track)
-                .ThenInclude(t => t.TrackPersonGroupPersonRelations)
-                .ThenInclude(r => r.PersonGroup)
-                .ThenInclude(g => g.PersonGroupStreamInfo)
-                .Include(t => t.TrackStreamInfo)
-                .ThenInclude(t => t.Track)
-                .ThenInclude(t => t.Disc)
-                .ThenInclude(d => d.Album)
-                .ThenInclude(a => a.AlbumPersonGroupPersonRelations)
-                .ThenInclude(r => r.Persons)
-                .Include(t => t.TrackStreamInfo)
-                .ThenInclude(t => t.Track)
-                .ThenInclude(t => t.Disc)
-                .ThenInclude(d => d.Album)
-                .ThenInclude(a => a.AlbumPersonGroupPersonRelations)
-                .ThenInclude(r => r.PersonGroup)
-                .ThenInclude(g => g.PersonGroupStreamInfo)
-                .OrderByDescending(t => t.Played)
-                .Take(100)
-                .AsSplitQuery()
+            Cambion.Register(this);
+
+            HandleEvent(new PlaylistUpdated());
+        }
+
+        // ReSharper disable once AsyncVoidMethod
+        public async void HandleEvent(PlaylistUpdated input)
+        {
+            try
+            {
+                DateTime now = SystemClock.Now;
+
+                if (SelectedDate.Date != now.Date)
+                {
+                    // Don't need to update the page when a track changes if the user is currently viewing history for a different date
+                    return;
+                }
+
+                await OnHistoryPageChanged(1);
+
+                await InvokeAsync(StateHasChanged);
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e, "{exceptionMessage}", e.Message);
+            }
+        }
+
+        private async Task OnDateChanged()
+        {
+            await OnHistoryPageChanged(1);
+        }
+
+        private async Task OnHistoryPageChanged(int page)
+        {
+            HistoryCurrentPage = page;
+            DateTime now = SystemClock.Now;
+
+            SegnoSharpDbContext dbContext = await DbFactory.CreateDbContextAsync();
+
+            HistoryTotalPages = (int)Math.Ceiling(await dbContext.StreamHistory
+                .AsNoTracking()
+                .Where(h => h.Played > SelectedDate.Date && h.Played < SelectedDate.Date.AddDays(1))
+                .CountAsync() / (double)HistoryPageSize);
+
+            StreamHistory currentlyPlaying = await dbContext.StreamHistory
+                .AsNoTracking()
+                .OrderByDescending(h => h.Played)
+                .FirstOrDefaultAsync(h => h.Played.AddSeconds(h.TrackStreamInfo.Track.Length) > now);
+
+
+            HistoryItems = await dbContext.StreamHistory
+                .AsNoTracking()
+                .Where(h => h.Played > SelectedDate.Date && h.Played < SelectedDate.Date.AddDays(1))
+                .OrderByDescending(h => h.Played)
+                .Skip(HistoryPageSize * (HistoryCurrentPage - 1))
+                .Take(HistoryPageSize)
+                .Select(h => new HistoryViewModel
+                {
+                    AlbumTitle = h.TrackStreamInfo.Track.Disc.Album.Title,
+                    TrackTitle = h.TrackStreamInfo.Track.Title,
+                    Length = h.TrackStreamInfo.Track.Duration,
+                    AlbumId = h.TrackStreamInfo.Track.Disc.Album.Id,
+                    HasAlbumCover = h.TrackStreamInfo.Track.Disc.Album.AlbumCover != null,
+                    Played = h.Played,
+                    CurrentlyPlaying = h.Id == currentlyPlaying.Id,
+                    TrackArtists = string.Join(", ",
+                        h.TrackStreamInfo.Track.TrackPersonGroupPersonRelations
+                            .Where(r =>
+                                r.PersonGroup.PersonGroupStreamInfo != null &&
+                                r.PersonGroup.PersonGroupStreamInfo.IncludeInAutoPlaylist)
+                            .SelectMany(r =>
+                                r.Persons.Select(p =>
+                                    p.FirstName == null ? p.LastName : p.FirstName + " " + p.LastName))),
+                    AlbumArtists = string.Join(", ",
+                        h.TrackStreamInfo.Track.Disc.Album.AlbumPersonGroupPersonRelations
+                            .Where(r =>
+                                r.PersonGroup.PersonGroupStreamInfo != null &&
+                                r.PersonGroup.PersonGroupStreamInfo.IncludeInAutoPlaylist)
+                            .SelectMany(r =>
+                                r.Persons.Select(p =>
+                                    p.FirstName == null ? p.LastName : p.FirstName + " " + p.LastName)))
+                })
                 .ToListAsync();
-
-            foreach (StreamHistory historyItem in history)
-            {
-
-                var artists = string.Empty;
-
-                string[] trackPeople = historyItem.TrackStreamInfo.Track.TrackPersonGroupPersonRelations
-                    .Where(g => g.PersonGroup.PersonGroupStreamInfo is { IncludeInAutoPlaylist: true })
-                    .SelectMany(g => g.Persons)
-                    .Distinct()
-                    .Select(p => (p.FirstName + " " + p.LastName).Trim())
-                    .ToArray();
-
-                if (trackPeople.Length > 0)
-                {
-                    artists = string.Join(", ", trackPeople);
-                }
-                else
-                {
-                    string[] albumPeople = historyItem.TrackStreamInfo.Track.Disc.Album.AlbumPersonGroupPersonRelations
-                        .Where(g => g.PersonGroup.PersonGroupStreamInfo is { IncludeInAutoPlaylist: true })
-                        .SelectMany(g => g.Persons)
-                        .Distinct()
-                        .Select(p => (p.FirstName + " " + p.LastName).Trim())
-                        .ToArray();
-
-                    if (albumPeople.Length > 0)
-                    {
-                        artists = string.Join(", ", albumPeople);
-                    }
-                }
-
-                HistoryViewModel vm = new()
-                {
-                    Album = historyItem.TrackStreamInfo.Track.Disc.Album.Title,
-                    Track = historyItem.TrackStreamInfo.Track.Title,
-                    Artist = artists,
-                    Played = historyItem.Played
-                };
-
-                HistoryItems.Add(vm);
-            }
-        }
-
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                _dbContext?.Dispose();
-            }
         }
     }
 }
---
+++ src/Modules/Playlist/ViewModels/HistoryViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Whitestone.SegnoSharp.Modules.Playlist.ViewModels
+{
+    internal class HistoryViewModel
+    {
+        public string AlbumTitle { get; set; }
+        public string TrackTitle { get; set; }
+        public string AlbumArtists { get; set; }
+        public string TrackArtists { get; set; }
+        public TimeSpan Length { get; set; }
+        public DateTime Played { get; set; }
+        public int AlbumId { get; set; }
+        public bool HasAlbumCover { get; set; }
+        public bool CurrentlyPlaying { get; set; }
+
+        public string Artists => string.IsNullOrEmpty(TrackArtists) ? AlbumArtists : TrackArtists;
+    }
+}
