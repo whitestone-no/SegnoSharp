97a4b128efde46fcb1f368d3b0b7718d202fe2c7
Gina Therese Hvidsten <8248605+gthvidsten@users.noreply.github.com>
2025-02-25 22:32:51 +0100

    Removed stream and playlist controls

--- src/SegnoSharp/Components/Pages/Index.razor.cs
+++ src/SegnoSharp/Components/Pages/Index.razor.cs
@@ -1,25 +1,6 @@
-﻿using Microsoft.AspNetCore.Components;
-using Whitestone.Cambion.Interfaces;
-using Whitestone.SegnoSharp.Common.Events;
-
-namespace Whitestone.SegnoSharp.Components.Pages
+﻿namespace Whitestone.SegnoSharp.Components.Pages
 {
     public partial class Index
     {
-        [Inject]
-        private ICambion Cambion { get; set; } = null!;
-
-        private async void FireEvent_PlayNextTrack_Click()
-        {
-            await Cambion.PublishEventAsync(new PlayNextTrack());
-        }
-        private async void FireEvent_StartStreaming_Click()
-        {
-            await Cambion.PublishEventAsync(new StartStreaming());
-        }
-        private async void FireEvent_StopStreaming_Click()
-        {
-            await Cambion.PublishEventAsync(new StopStreaming());
-        }
     }
 }
