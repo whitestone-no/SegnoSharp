17e2908b57e0fa6da1ad2768b3dc6836dd662f88
Gina Therese Hvidsten <8248605+gthvidsten@users.noreply.github.com>
2025-02-27 20:41:43 +0100

    Change port to be ushort (as that has a valid port range), and add friendly names to enum values

--- src/Common/Models/StreamingSettings.cs
+++ src/Common/Models/StreamingSettings.cs
@@ -1,11 +1,13 @@
-﻿namespace Whitestone.SegnoSharp.Common.Models
+﻿using Whitestone.SegnoSharp.Common.Attributes.PersistenceManager;
+
+namespace Whitestone.SegnoSharp.Common.Models
 {
     public class StreamingSettings
     {
         public virtual AudioFormat AudioFormat { get; set; }
         public virtual Bitrate Bitrate { get; set; }
         public virtual string Hostname { get; set; }
-        public virtual int Port { get; set; }
+        public virtual ushort Port { get; set; }
         public virtual string MountPoint { get; set; }
         public virtual string Password { get; set; }
         public virtual bool IsPublic { get; set; }
@@ -17,16 +19,23 @@
 
     public enum Bitrate
     {
+        [FriendlyName("64 kbps")]
         Kbps64 = 64,
+        [FriendlyName("128 kbps")]
         Kbps128 = 128,
+        [FriendlyName("192 kbps")]
         Kbps192 = 192,
+        [FriendlyName("256 kbps")]
         Kbps256 = 256,
+        [FriendlyName("320 kbps")]
         Kbps320 = 320
     }
 
     public enum AudioFormat
     {
+        [FriendlyName("MP3")]
         Mp3,
+        [FriendlyName("AAC")]
         Aac
     }
 
